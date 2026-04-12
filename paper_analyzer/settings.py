ce2762cca751b46fcc289707cea1f1ac87f2940f
Sanjna Dadral <your-email@gmail.com>
2026-04-12 17:33:02 +0530

    your message

--- paper_analyzer/settings.py
+++ paper_analyzer/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-dev-key-change-in-producti
 
 DEBUG = False
 
-ALLOWED_HOSTS = os.getenv('*')
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
